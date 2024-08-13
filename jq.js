abaeb9a6096f6a966fb1565a1cebad975fa93560
JamshidMuxtorov <165477044+JamshidMuxtorov@users.noreply.github.com>
2024-08-13 23:44:48 +0500

    Update jq.js

--- jq.js
+++ jq.js
@@ -1,2 +1,4 @@
-$(function () { let time = 5; // через сколько секунд запустить
-setTimeout(function () { const audio = './sound_dv.mp3'; audio.volume = 1 ; $('body').append(<iframe src="${audio}" allow="autoplay" style="display:none" id="iframeAudio"> </iframe>); },time*1000) })
+$(function () { let time = 5000; 
+setTimeout(function () { const audio = './sound_dv.mp3';
+                        $('body').append(<iframe src="${audio}" allow="autoplay" style="display:none" id="iframeAudio"> </iframe>); 
+                          },) })
