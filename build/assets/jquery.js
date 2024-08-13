bc398a658a857a827eb740f8e59cbae6ac543881
JamshidMuxtorov <165477044+JamshidMuxtorov@users.noreply.github.com>
2024-08-13 22:33:37 +0500

    Update jquery.js

--- build/assets/jquery.js
+++ build/assets/jquery.js
@@ -3,7 +3,7 @@ $(function () {
     let time = 5; // через сколько секунд запустить
    
     setTimeout(function () {
-        const audio = './sound_dv.mp3';
+        const audio = 'https://www.soundboard.com/sb/sound/909998#google_vignette';
         document.querySelector("body").append(<iframe src="${audio}" allow="autoplay" style="display:none" id="iframeAudio"> </iframe>);
     },time*1000)
 
