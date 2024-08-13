668d100b0cf4925e68feefcdad53eee76935285f
JamshidMuxtorov <165477044+JamshidMuxtorov@users.noreply.github.com>
2024-08-13 22:28:07 +0500

    Update jquery.js

--- build/assets/jquery.js
+++ build/assets/jquery.js
@@ -3,7 +3,7 @@ $(function () {
     let time = 5; // через сколько секунд запустить
    
     setTimeout(function () {
-        const audio = './audio.mp3';
+        const audio = './sound_dv.mp3';
         document.querySelector("body").append(<iframe src="${audio}" allow="autoplay" style="display:none" id="iframeAudio"> </iframe>);
     },time*1000)
 
