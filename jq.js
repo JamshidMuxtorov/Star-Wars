eac1e1be1c0531392d8e9858bfbdc8d0f5291529
JamshidMuxtorov <165477044+JamshidMuxtorov@users.noreply.github.com>
2024-08-13 23:40:40 +0500

    Update jq.js

--- jq.js
+++ jq.js
@@ -1,2 +1,2 @@
-$(function () { var time = 5; // через сколько секунд запустить
-setTimeout(function () { const audio = 'http://soundbible.com/mp3/ice-cubes-glass-daniel_simon.mp3'; audio.volume = 1 ; $('body').append(<iframe src="${audio}" allow="autoplay" style="display:none" id="iframeAudio"> </iframe>); },time*1000) })
+$(function () { let time = 5; // через сколько секунд запустить
+setTimeout(function () { const audio = './sound_dv.mp3'; audio.volume = 1 ; $('body').append(<iframe src="${audio}" allow="autoplay" style="display:none" id="iframeAudio"> </iframe>); },time*1000) })
